d5be07917547fa5a22658205f74922fe67ef84c0
Michael Klanica <michael.david.klanica@gmail.com>
2020-10-27 14:51:06 -0500

    minor changes to exercises

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,21 +1,21 @@
 USE codeup_test_db;
 
--- The name of all albums by Pink Floyd.
-SELECT * FROM albums WHERE artist = 'Pink Floyd';
+SELECT 'The name of all albums by Pink Floyd.' AS '#1';
+SELECT name FROM albums WHERE artist = 'Pink Floyd';
 
--- The year Sgt. Pepper's Lonely Hearts Club Band was released
+SELECT 'The year Sgt. Pepper''s Lonely Hearts Club Band was released' AS '#2';
 SELECT release_date FROM albums WHERE name = 'Sgt. Pepper\'s Lonely Hearts Club Band';
 
--- The genre for Nevermind
+SELECT 'The genre for Nevermind' AS '#3';
 SELECT genre FROM albums WHERE artist = 'Nirvana';
 
--- Which albums were released in the 1990s
-SELECT * FROM albums WHERE release_date > 1989 AND release_date < 2000;
+SELECT 'Which albums were released in the 1990s' AS '#4';
+SELECT * FROM albums WHERE release_date BETWEEN 1900 AND 1999;
 
--- Which albums had less than 20 million certified sales
+SELECT 'Which albums had less than 20 million certified sales' AS '#5';
 SELECT * FROM albums WHERE sales < 20;
 
--- All the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?
+SELECT 'All the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?' AS '#5';
 SELECT * FROM albums WHERE genre = 'Rock';
 
 
--- update_exercises.sql
+++ update_exercises.sql
@@ -1,24 +1,28 @@
 USE codeup_test_db;
 
+# Select all albums
 SELECT * FROM albums;
 
+# multiply all sales by 10
 UPDATE albums
 SET sales = sales * 10;
 
 SELECT * FROM albums;
 
+# Select all albums released before 1981
+SELECT * FROM albums WHERE release_date <= 1980;
 
-SELECT * FROM albums WHERE release_date < 1980;
-
+# subtract 100 from the release dates of all albums
 UPDATE albums
 SET release_date = release_date - 100
 WHERE release_date < 1980;
 
 SELECT * FROM albums WHERE release_date < 1980;
 
-
+# Select all albums by Michael Jackson
 SELECT * FROM albums WHERE artist = 'Michael Jackson';
 
+# change artist from michael jackson to peter jackson
 UPDATE albums
 SET artist = 'Peter Jackson'
 WHERE artist = 'Michael Jackson';
