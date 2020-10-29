138a75bf09ee5f0abfa235d0b9063f0282d46e9d
Michael Klanica <michael.david.klanica@gmail.com>
2020-10-29 16:36:44 -0500

    SQL exercises - JOIN

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,8 +1,17 @@
 USE employees;
-SELECT CONCAT(e.first_name, ' ', e.last_name) AS full_name, d.dept_name
+SELECT d.dept_name, CONCAT(e.first_name, ' ', e.last_name) AS full_name
 FROM employees as e
-         JOIN dept_emp as de
-              ON de.emp_no = e.emp_no
+         JOIN dept_manager as dm
+              ON dm.emp_no = e.emp_no
          JOIN departments as d
-              ON d.dept_no = de.dept_no
-WHERE de.to_date = '9999-01-01' AND e.emp_no = 10001;
+              ON d.dept_no = dm.dept_no
+WHERE dm.to_date = '9999-01-01';
+
+SELECT d.dept_name, CONCAT(e.first_name, ' ', e.last_name) AS full_name
+FROM employees as e
+         JOIN dept_manager as dm
+              ON dm.emp_no = e.emp_no
+         JOIN departments as d
+              ON d.dept_no = dm.dept_no
+WHERE e.gender = 'f'
+  AND dm.to_date = '9999-01-01';
