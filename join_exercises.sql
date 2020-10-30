8ea58e78a3f09252999118689cf9495311bd3611
Michael Klanica <michael.david.klanica@gmail.com>
2020-10-30 12:26:59 -0500

    SQL exercise - JOIN

--- join_exercises.sql
+++ join_exercises.sql
@@ -15,3 +15,13 @@ FROM employees as e
               ON d.dept_no = dm.dept_no
 WHERE e.gender = 'f'
   AND dm.to_date = '9999-01-01';
+
+
+SELECT t.title as Title, COUNT(t.emp_no) AS Count
+FROM titles AS t
+JOIN dept_emp AS de ON t.emp_no = de.emp_no
+JOIN departments AS d ON de.dept_no = d.dept_no
+WHERE de.dept_no = 'd009'
+AND t.to_date = '9999-01-01'
+AND de.to_date = '9999-01-01'
+GROUP BY t.title;
